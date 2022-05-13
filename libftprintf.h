7e93f8690409ed7ddce9079fb84bcaec2a1d9d35
ThzEdouard <edouard.flaquet@gmail.com>
2022-05-13 12:16:36 +0200

    firt

---
+++ libftprintf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eflaquet <eflaquet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/13 11:44:41 by eflaquet          #+#    #+#             */
+/*   Updated: 2022/05/13 12:11:45 by eflaquet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include "libft/libft.h"
+# include <stdarg.h>
+
+int	ft_printf(const char *, ...);
+
+
+#endif
